49cce18a195f3d05626fc2d4da89c657ade35ec9
Sigmund Onochie-Mordi <sigmund.onochie-mordi@pau.edu.ng>
2023-11-05 11:28:16 +0100

    (My second commit)

---
+++ Week2/project-2.rs
@@ -0,0 +1,14 @@
+  fn main() {
+  	 let t = 900_000.0;
+  	 let m = 1_500_000.0;
+  	 let h = 2_250_000.0;
+  	 let d = 8_550_000.0;
+  	 let a = 250_000.0;
+
+  	 // sum
+  	 let s = t + m + h + d + a;
+  	 println!("Sum is {}", s);
+  	 let ave = s / 10.0;
+  	 println!("Average is {}", ave);
+
+  }
