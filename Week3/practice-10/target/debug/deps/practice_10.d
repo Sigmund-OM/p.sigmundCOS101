495e342a38648c48cb44ab201daa48e1ed94eb0f
Sigmund Onochie-Mordi <sigmund.onochie-mordi@pau.edu.ng>
2023-11-05 17:49:00 +0100

    (My third commit)

---
+++ Week3/practice-10/target/debug/deps/practice_10.d
@@ -0,0 +1,5 @@
+C:\Users\sigmu\Documents\p.sigmundCOS101\Week3\practice-10\target\debug\deps\practice_10.exe: src\main.rs
+
+C:\Users\sigmu\Documents\p.sigmundCOS101\Week3\practice-10\target\debug\deps\practice_10.d: src\main.rs
+
+src\main.rs:
